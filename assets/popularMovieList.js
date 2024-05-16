2affde98b6c34b8ce95e8c5f09df10436c837996
Yusufcan Senturk <developer.yusufcan@gmail.com>
2024-05-16 19:48:43 +0300

    Added popularMovieList.js file

---
+++ assets/popularMovieList.js
@@ -0,0 +1,402 @@
+export default 
+[
+        {
+            "adult": false,
+            "backdrop_path": "/qrGtVFxaD8c7et0jUtaYhyTzzPg.jpg",
+            "genre_ids": [
+                878,
+                28,
+                12
+            ],
+            "id": 823464,
+            "original_language": "en",
+            "original_title": "Godzilla x Kong: The New Empire",
+            "overview": "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.",
+            "popularity": 4738.497,
+            "poster_path": "/tMefBSflR6PGQLv7WvFPpKLZkyk.jpg",
+            "release_date": "2024-03-27",
+            "title": "Godzilla x Kong: The New Empire",
+            "video": false,
+            "vote_average": 7.033,
+            "vote_count": 1455
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/fypydCipcWDKDTTCoPucBsdGYXW.jpg",
+            "genre_ids": [
+                878,
+                12,
+                28
+            ],
+            "id": 653346,
+            "original_language": "en",
+            "original_title": "Kingdom of the Planet of the Apes",
+            "overview": "Several generations in the future following Caesar's reign, apes are now the dominant species and live harmoniously while humans have been reduced to living in the shadows. As a new tyrannical ape leader builds his empire, one young ape undertakes a harrowing journey that will cause him to question all that he has known about the past and to make choices that will define a future for apes and humans alike.",
+            "popularity": 2181.36,
+            "poster_path": "/gKkl37BQuKTanygYQG1pyYgLVgf.jpg",
+            "release_date": "2024-05-08",
+            "title": "Kingdom of the Planet of the Apes",
+            "video": false,
+            "vote_average": 7.258,
+            "vote_count": 345
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/fY3lD0jM5AoHJMunjGWqJ0hRteI.jpg",
+            "genre_ids": [
+                878,
+                27,
+                28
+            ],
+            "id": 940721,
+            "original_language": "ja",
+            "original_title": "ゴジラ-1.0",
+            "overview": "Postwar Japan is at its lowest point when a new crisis emerges in the form of a giant monster, baptized in the horrific power of the atomic bomb.",
+            "popularity": 1722.666,
+            "poster_path": "/hkxxMIGaiCTmrEArK7J56JTKUlB.jpg",
+            "release_date": "2023-11-03",
+            "title": "Godzilla Minus One",
+            "video": false,
+            "vote_average": 7.66,
+            "vote_count": 1024
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/piLUbWQ3pgkma1nCyjHLEoMCSsN.jpg",
+            "genre_ids": [
+                18,
+                10770
+            ],
+            "id": 1219685,
+            "original_language": "fr",
+            "original_title": "Un père idéal",
+            "overview": "",
+            "popularity": 1362.446,
+            "poster_path": "/4xJd3uwtL1vCuZgEfEc8JXI9Uyx.jpg",
+            "release_date": "2024-04-21",
+            "title": "Un père idéal",
+            "video": false,
+            "vote_average": 6.278,
+            "vote_count": 18
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/5cCfqeUH2f5Gnu7Lh9xepY9TB6x.jpg",
+            "genre_ids": [
+                14,
+                12,
+                35
+            ],
+            "id": 967847,
+            "original_language": "en",
+            "original_title": "Ghostbusters: Frozen Empire",
+            "overview": "After the original team developed a top-secret research laboratory to take the shattered ghosts to the next level. But when the discovery of an ancient artifact unleashes an evil force, both new and old must join forces to protect their homeland and save the world from the Second Ice Age... from the very thing that unleashes an unstoppable force from the depths of history. As chaos reigns and pure evil plunges the world into darkness, the Ghostbusters must step forward once again to save humanity from the brink of doom. With two generations of Ghostbusters fighting side by side, the stakes have never been higher. Will they be able to avert the inevitable before everything freezes?",
+            "popularity": 1060.891,
+            "poster_path": "/e1J2oNzSBdou01sUvriVuoYp0pJ.jpg",
+            "release_date": "2024-03-20",
+            "title": "Ghostbusters: Frozen Empire",
+            "video": false,
+            "vote_average": 6.756,
+            "vote_count": 705
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/xOMo8BRK7PfcJv9JCnx7s5hj0PX.jpg",
+            "genre_ids": [
+                878,
+                12
+            ],
+            "id": 693134,
+            "original_language": "en",
+            "original_title": "Dune: Part Two",
+            "overview": "Follow the mythic journey of Paul Atreides as he unites with Chani and the Fremen while on a path of revenge against the conspirators who destroyed his family. Facing a choice between the love of his life and the fate of the known universe, Paul endeavors to prevent a terrible future only he can foresee.",
+            "popularity": 1083.039,
+            "poster_path": "/gho58bYmw9juYXmUSHRJKOngJGn.jpg",
+            "release_date": "2024-02-27",
+            "title": "Dune: Part Two",
+            "video": false,
+            "vote_average": 8.202,
+            "vote_count": 3885
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/ySgY4jBvZ6qchrxKnBg4M8tZp8V.jpg",
+            "genre_ids": [
+                27,
+                53
+            ],
+            "id": 1111873,
+            "original_language": "en",
+            "original_title": "Abigail",
+            "overview": "A group of criminals kidnaps a teenage ballet dancer, the daughter of a notorious gang leader, in order to obtain a ransom of $50 million, but over time, they discover that she is not just an ordinary girl. After the kidnappers begin to diminish, one by one, they discover, to their increasing horror, that they are locked inside with an unusual girl.",
+            "popularity": 938.879,
+            "poster_path": "/7qxG0zyt29BI0IzFDfsps62kbQi.jpg",
+            "release_date": "2024-04-18",
+            "title": "Abigail",
+            "video": false,
+            "vote_average": 6.961,
+            "vote_count": 380
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/kYgQzzjNis5jJalYtIHgrom0gOx.jpg",
+            "genre_ids": [
+                16,
+                28,
+                10751,
+                35,
+                14
+            ],
+            "id": 1011985,
+            "original_language": "en",
+            "original_title": "Kung Fu Panda 4",
+            "overview": "Po is gearing up to become the spiritual leader of his Valley of Peace, but also needs someone to take his place as Dragon Warrior. As such, he will train a new kung fu practitioner for the spot and will encounter a villain called the Chameleon who conjures villains from the past.",
+            "popularity": 994.511,
+            "poster_path": "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg",
+            "release_date": "2024-03-02",
+            "title": "Kung Fu Panda 4",
+            "video": false,
+            "vote_average": 7.128,
+            "vote_count": 1648
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/6Xfj8wD7GoEysgcFayRKd6QLqai.jpg",
+            "genre_ids": [
+                10749,
+                35,
+                18
+            ],
+            "id": 843527,
+            "original_language": "en",
+            "original_title": "The Idea of You",
+            "overview": "Solène, a 40-year-old single mom, begins an unexpected romance with 24-year-old Hayes Campbell, the lead singer of August Moon, the hottest boy band on the planet. When Solène must step in to chaperone her teenage daughter's trip to the Coachella Music Festival after her ex bails at the last minute, she has a chance encounter with Hayes and there is an instant, undeniable spark. As they begin a whirlwind romance, it isn't long before Hayes' superstar status poses unavoidable challenges to their relationship, and Solène soon discovers that life in the glare of his spotlight might be more than she bargained for.",
+            "popularity": 815.407,
+            "poster_path": "/zDi2U7WYkdIoGYHcYbM9X5yReVD.jpg",
+            "release_date": "2024-05-02",
+            "title": "The Idea of You",
+            "video": false,
+            "vote_average": 7.503,
+            "vote_count": 699
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/jnE1GA7cGEfv5DJBoU2t4bZHaP4.jpg",
+            "genre_ids": [
+                28,
+                878
+            ],
+            "id": 1094844,
+            "original_language": "en",
+            "original_title": "Ape vs. Mecha Ape",
+            "overview": "Recognizing the destructive power of its captive giant Ape, the military makes its own battle-ready A.I., Mecha Ape. But its first practical test goes horribly wrong, leaving the military no choice but to release the imprisoned giant ape to stop the colossal robot before it destroys downtown Chicago.",
+            "popularity": 907.094,
+            "poster_path": "/dJaIw8OgACelojyV6YuVsOhtTLO.jpg",
+            "release_date": "2023-03-24",
+            "title": "Ape vs. Mecha Ape",
+            "video": false,
+            "vote_average": 5.61,
+            "vote_count": 172
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/7kQvHmGyKJv2wSKVfCUux50rd7A.jpg",
+            "genre_ids": [
+                878
+            ],
+            "id": 920342,
+            "original_language": "zh",
+            "original_title": "异兽战场",
+            "overview": "The soldier king Qin Yang's fiancée Ye Qin met with an unknown beast and died tragically. Gu Ping invites him to participate in Ye Qin's scientific research before her death. But Gu Ping is using Ye Qin's research results to combine the genes of unknown beasts to create the \"Zero\" dragon creature. The intelligent dragon creature, coupled with the extra-terrestrial beast evolved by devouring,  an imminent city war is coming...",
+            "popularity": 859.043,
+            "poster_path": "/nBVYp2xxx2R02n21EGlDky8CgWR.jpg",
+            "release_date": "2021-12-27",
+            "title": "Monster's Battlefield",
+            "video": false,
+            "vote_average": 7.364,
+            "vote_count": 11
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/4TFCA5Lm5miseV3Wy02yxurEQL9.jpg",
+            "genre_ids": [
+                878,
+                28,
+                18,
+                53
+            ],
+            "id": 119450,
+            "original_language": "en",
+            "original_title": "Dawn of the Planet of the Apes",
+            "overview": "A group of scientists in San Francisco struggle to stay alive in the aftermath of a plague that is wiping out humanity, while Caesar tries to maintain dominance over his community of intelligent apes.",
+            "popularity": 789.009,
+            "poster_path": "/kScdQEwS9jPEdnO23XjGAtaoRcT.jpg",
+            "release_date": "2014-07-08",
+            "title": "Dawn of the Planet of the Apes",
+            "video": false,
+            "vote_average": 7.317,
+            "vote_count": 10856
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/4woSOUD0equAYzvwhWBHIJDCM88.jpg",
+            "genre_ids": [
+                28,
+                27,
+                53
+            ],
+            "id": 1096197,
+            "original_language": "en",
+            "original_title": "No Way Up",
+            "overview": "Characters from different backgrounds are thrown together when the plane they're travelling on crashes into the Pacific Ocean. A nightmare fight for survival ensues with the air supply running out and dangers creeping in from all sides.",
+            "popularity": 766.654,
+            "poster_path": "/hu40Uxp9WtpL34jv3zyWLb5zEVY.jpg",
+            "release_date": "2024-01-18",
+            "title": "No Way Up",
+            "video": false,
+            "vote_average": 6.4,
+            "vote_count": 648
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/x1Ex090BNIXcmaBYB2US0j3hBUF.jpg",
+            "genre_ids": [
+                28,
+                53,
+                18
+            ],
+            "id": 1057001,
+            "original_language": "ko",
+            "original_title": "The Childe",
+            "overview": "Desperate to secure funds for his mother's surgery, an amateur boxer searches for the wealthy father he's never met. Traveling from the Philippines to Korea, he is relentlessly pursued and forced to confront a shocking truth.",
+            "popularity": 659.453,
+            "poster_path": "/ydC8ubFXQfkPJDhkduMhbE9mTz7.jpg",
+            "release_date": "2023-06-21",
+            "title": "The Childe",
+            "video": false,
+            "vote_average": 7.063,
+            "vote_count": 80
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/tpiqEVTLRz2Mq7eLq5DT8jSrp71.jpg",
+            "genre_ids": [
+                878,
+                28,
+                18
+            ],
+            "id": 934632,
+            "original_language": "en",
+            "original_title": "Rebel Moon - Part Two: The Scargiver",
+            "overview": "The rebels gear up for battle against the ruthless forces of the Motherworld as unbreakable bonds are forged, heroes emerge — and legends are made.",
+            "popularity": 671.228,
+            "poster_path": "/cxevDYdeFkiixRShbObdwAHBZry.jpg",
+            "release_date": "2024-04-19",
+            "title": "Rebel Moon - Part Two: The Scargiver",
+            "video": false,
+            "vote_average": 6.098,
+            "vote_count": 750
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/hU1Q9YVzdYhokr8a9gLywnSUMlN.jpg",
+            "genre_ids": [
+                16,
+                12,
+                28,
+                14,
+                10751
+            ],
+            "id": 1147400,
+            "original_language": "fr",
+            "original_title": "Miraculous World : Paris, Les Aventures de Toxinelle et Griffe Noire",
+            "overview": "Miraculous holders from another world appear in Paris. They come from a parallel universe where everything is reversed: the holders of Ladybug and Black Cat Miraculouses, Shadybug and Claw Noir, are the bad guys, and the holder of the Butterfly Miraculous, Hesperia, is a superhero. Ladybug and Cat Noir will have to help Hesperia counter the attacks of their evil doubles and prevent them from seizing the Butterfly's Miraculous. Can our heroes also help Hesperia make Shadybug and Claw Noir better people?",
+            "popularity": 715.466,
+            "poster_path": "/7Md3nuV0ZprBTnkdR3OrUCEsrSP.jpg",
+            "release_date": "2023-10-21",
+            "title": "Miraculous World: Paris, Tales of Shadybug and Claw Noir",
+            "video": false,
+            "vote_average": 7.68,
+            "vote_count": 139
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/v5XyXZe8FADw8iHupB4L7QOAwH9.jpg",
+            "genre_ids": [
+                16,
+                35,
+                10751
+            ],
+            "id": 748783,
+            "original_language": "en",
+            "original_title": "The Garfield Movie",
+            "overview": "Garfield, the world-famous, Monday-hating, lasagna-loving indoor cat, is about to have a wild outdoor adventure! After an unexpected reunion with his long-lost father – scruffy street cat Vic – Garfield and his canine friend Odie are forced from their perfectly pampered life into joining Vic in a hilarious, high-stakes heist.",
+            "popularity": 658.923,
+            "poster_path": "/zK2sFxZcelHJRPVr242rxy5VK4T.jpg",
+            "release_date": "2024-04-30",
+            "title": "The Garfield Movie",
+            "video": false,
+            "vote_average": 6.667,
+            "vote_count": 66
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/rmNlWyez5cniGtXkgixG1ezdqVk.jpg",
+            "genre_ids": [
+                28,
+                53
+            ],
+            "id": 1093995,
+            "original_language": "en",
+            "original_title": "Chief of Station",
+            "overview": "After learning that the death of his wife was not an accident, a former CIA Station Chief is forced back into the espionage underworld, teaming up with an adversary to unravel a conspiracy that challenges everything he thought he knew.",
+            "popularity": 914.165,
+            "poster_path": "/uuA01PTtPombRPvL9dvsBqOBJWm.jpg",
+            "release_date": "2024-05-02",
+            "title": "Chief of Station",
+            "video": false,
+            "vote_average": 5.125,
+            "vote_count": 24
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/nb3xI8XI3w4pMVZ38VijbsyBqP4.jpg",
+            "genre_ids": [
+                18,
+                36
+            ],
+            "id": 872585,
+            "original_language": "en",
+            "original_title": "Oppenheimer",
+            "overview": "The story of J. Robert Oppenheimer's role in the development of the atomic bomb during World War II.",
+            "popularity": 609.4,
+            "poster_path": "/8Gxv8gSFCU0XGDykEGv7zR1n2ua.jpg",
+            "release_date": "2023-07-19",
+            "title": "Oppenheimer",
+            "video": false,
+            "vote_average": 8.102,
+            "vote_count": 8020
+        },
+        {
+            "adult": false,
+            "backdrop_path": "/qwK9soQmmJ7kRdjLZVXblw3g7AQ.jpg",
+            "genre_ids": [
+                28,
+                12,
+                53,
+                80
+            ],
+            "id": 7451,
+            "original_language": "en",
+            "original_title": "xXx",
+            "overview": "Xander Cage is your standard adrenaline junkie with no fear and a lousy attitude. When the US Government \"recruits\" him to go on a mission, he's not exactly thrilled. His mission: to gather information on an organization that may just be planning the destruction of the world, led by the nihilistic Yorgi.",
+            "popularity": 621.575,
+            "poster_path": "/xeEw3eLeSFmJgXZzmF2Efww0q3s.jpg",
+            "release_date": "2002-08-09",
+            "title": "xXx",
+            "video": false,
+            "vote_average": 5.932,
+            "vote_count": 4049
+        }
+
+]
